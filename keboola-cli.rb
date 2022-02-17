cc6fe599e1a55a73806004532db92bf8a817581a
keboola <>
2022-02-17 14:37:42 +0000

    Update from https://github.com/keboola/keboola-as-code/commit/8e8d5ec76fd6f44fc34f91a1b63aeb038bad6483

--- keboola-cli.rb
+++ keboola-cli.rb
@@ -4,24 +4,24 @@ class KeboolaCli < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.0_darwin_arm64.zip"
-    sha256 "c876dee230843a46148cb4d39cc454144f6a76603e1a5fca004555167d14f1bf"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.1_darwin_arm64.zip"
+    sha256 "af1219726f27f5ec87cfb91feb11bdf42ee88c4d10483520a05eee5cf0883ab4"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.0_darwin_amd64.zip"
-    sha256 "ffe112a850451a39c37bf0af1c179560ee78981c9e4bccf7499f83317c2baf4f"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.1_darwin_amd64.zip"
+    sha256 "7ac8511496ead5d35402fd8b64f4ad349b0d1e168919bbf320ff0b683ca5913b"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.0_linux_amd64.zip"
-    sha256 "f7be4f7948965f401a17dd0d553b893fd33a4e9984d44f52f8ea5380eb08313f"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.1_linux_amd64.zip"
+    sha256 "b27c91ecea6421ac5c53e18904bf703532942847020d689c96bee267ee7166ef"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.0_linux_armv6.zip"
-    sha256 "5e3df64ee5fc9344e77539456a8edff0f6c70a1f4026245aee1ce3974a313553"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.1_linux_armv6.zip"
+    sha256 "08e9d1723f2d5447c22f3292f3a641f259dfeefcafb9b2154671dec762f9551c"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.0_linux_arm64.zip"
-    sha256 "2bf9f2b6d295220a03b08983c65729e498e1f65775d44ee003acb1fe3f62f26a"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.2.1_linux_arm64.zip"
+    sha256 "6a918bcdca4a24b8f2762f195eb08589c3e7661377a4b67dd313fe64f88f107a"
   end
 
   depends_on "git" => :recommended
