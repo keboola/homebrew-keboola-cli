35847c4b96c4040a19e980406a9a7319eb9b2be0
keboola <>
2023-12-21 13:09:42 +0000

    Update from https://github.com/keboola/keboola-as-code/commit/0a1c4a72e6ef539a8289960edc6dfb6d1824fcbf

--- keboola-cli.rb
+++ keboola-cli.rb
@@ -1,28 +1,28 @@
 class KeboolaCli < Formula
   desc "Keboola CLI tool"
   homepage "https://www.keboola.com/product/cli"
-  license "MIT"
-  version "2.20.0"
+  license "Apache2"
+  version "2.20.1"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.0_darwin_arm64.zip"
-    sha256 ""
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.1_darwin_arm64.zip"
+    sha256 "0d907b36fc9db98e09644e7df1cffd9b3d61d404bc059bd81e433bd951174d99"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.0_darwin_amd64.zip"
-    sha256 "90c75080ed89467db2f2e034c894559c522c9639f3a18cecdcff09f951178607"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.1_darwin_amd64.zip"
+    sha256 "3f70eac658b773f37fb34507695fe2df7393b28c2ccdbdbed905b9fb98114d54"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.0_linux_amd64.zip"
-    sha256 "bffe36d0d1d58d74326006ee2338e52d356a88df3f5572237f967594c8e0408e"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.1_linux_amd64.zip"
+    sha256 "4a1c007fa86a4ed3734a7dfc86c1b349fd7dc7368038f0b2070bfef462a3f271"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.0_linux_armv6.zip"
-    sha256 "a5d41b7ae1edcb77ae60ea77e1e61bcc347f585b8e546914339068e0baf0e6b8"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.1_linux_armv6.zip"
+    sha256 "a4df1b89b6c39f4704aedb77aea33c11468ed5f43f484797f9097d627b8162ae"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.0_linux_arm64.zip"
-    sha256 "3c03decbc0cb418293ec9c70e450301267b900fba9fadbea41cde1018b29bc32"
+    url "https://cli-dist.keboola.com/zip/keboola-cli_2.20.1_linux_arm64.zip"
+    sha256 "fa37ff798175dbaa20e4a9d19b04fce3b72813027d70b3b7d009516abc5efc55"
   end
 
   depends_on "git" => :recommended
